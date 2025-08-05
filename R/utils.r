a541681472fef4b63d25ee3724767886c7c95b4e
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2025-08-05 17:02:55 -0300

    Init utils com fun p validar nomes de tabelas

---
+++ R/utils.r
@@ -0,0 +1,23 @@
+
+valid_tabelas <- function(tabelas) {
+    ref <- c(
+        "usinas",
+        "potencia_disponivel_observada",
+        "geracao_observada",
+        "corte_observado",
+        "irradiancia_prevista",
+        "melhor_historico_geracao"
+    )
+
+    is_valid <- tabelas %in% ref
+
+    if (!all(is_valid)) {
+        invalidas <- tabelas[!is_valid]
+        invalidas <- paste0(invalidas, collapse = ",")
+        msg <- paste0("Tabelas [", invalidas, "] nao sao reconhecidas por `pfvIO`")
+        stop(msg)
+    }
+
+    invisible(NULL)
+}
+
