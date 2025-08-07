beb49da6af8ecc2781c8fe69178970f8166f5cf4
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2025-08-07 11:57:09 -0300

    Add testes de validadores

---
+++ tests/testthat/test-validacao.r
@@ -0,0 +1,55 @@
+test_that("valida_nomes_colunas", {
+
+    dd <- fread(system.file("extdata/usinas.csv", package = "pfvIO"))
+    nomes <- c("id_usina", "latitude", "longitude", "capacidade_instalada_MW", "data_inicio_operacao_comercial")
+    expect_no_error(valida_nomes_colunas(dd, nomes))
+
+    nomes[1] <- "erro"
+    expect_error(valida_nomes_colunas(dd, nomes))
+
+    nomes <- nomes[-1]
+    expect_no_error(valida_nomes_colunas(dd, nomes))
+})
+
+test_that("valida_tipos_colunas", {
+
+    dd <- fread(system.file("extdata/usinas.csv", package = "pfvIO"))
+    tipos <- structure(
+        c("character", "numeric", "numeric", "numeric", "POSIXct"),
+        names = c("id_usina", "latitude", "longitude", "capacidade_instalada_MW",
+            "data_inicio_operacao_comercial")
+    )
+    expect_no_error(valida_tipos_colunas(dd, tipos))
+
+    tipos[1] <- "erro"
+    expect_error(valida_tipos_colunas(dd, tipos))
+
+    tipos <- tipos[-1]
+    expect_no_error(valida_tipos_colunas(dd, tipos))
+
+    # garante que inteiros sao entendidos como numerico tambem
+    dd[, capacidade_instalada_MW := as.integer(capacidade_instalada_MW)]
+    expect_no_error(valida_tipos_colunas(dd, tipos))
+})
+
+test_that("valida_limites_colunas", {
+
+    dd <- fread(system.file("extdata/usinas.csv", package = "pfvIO"))
+    limites <- structure(
+        list(latitude = c(-90, 90), longitude = c(-180, 180), capacidade_instalada_MW = c(0, Inf)),
+        names = c("latitude", "longitude", "capacidade_instalada_MW")
+    )
+    expect_no_error(valida_limites_colunas(dd, limites))
+
+    limites[[1]] <- "erro"
+    expect_error(valida_limites_colunas(dd, limites))
+
+    limites[[1]] <- 1
+    expect_error(valida_limites_colunas(dd, limites))
+
+    limites[[1]] <- c(1:10)
+    expect_error(valida_limites_colunas(dd, limites))
+
+    limites <- limites[-1]
+    expect_no_error(valida_limites_colunas(dd, limites))
+})
