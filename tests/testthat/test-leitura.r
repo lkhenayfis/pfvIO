7949eb8ce8ed7c4281d24c759e5c23a4abf84601
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2025-09-23 11:02:24 -0300

    Descentraliza testes de leitura em getters

--- tests/testthat/test-leitura.r
+++ tests/testthat/test-leitura.r
@@ -1,83 +1,115 @@
-test_that("get_dataset", {
+# estes testes sao propositalmente muito simples, uma vez que a parte de validacao ja e testada
+# de forma dedicada e o backend `dbinterface` tambem e exaustivamente testado por si so
 
-    # local
-
-    conn <- conectamock_pfv(system.file("extdata/", package = "pfvIO"))
-    dataset <- get_dataset(conn, id_usina = "BAUFI3")
-
+test_that("get_usinas", {
+    conn_local <- conectamock_pfv(system.file("extdata/", package = "pfvIO"))
+    dt <- get_usinas(conn_local, id_usina = "BAUFI3")
     expect_identical(
-        dataset$usinas,
-        dbinterface::getfromdb(conn, "usinas", id_usina = "BAUFI3")
+        dt,
+        dbinterface::getfromdb(conn_local, "usinas", id_usina = "BAUFI3")
     )
 
+    conn_s3 <- conectamock_pfv("s3://ons-pem-historico/solar/pfvIO-teste")
+    dt <- get_usinas(conn_s3, id_usina = "BAUFI3")
     expect_identical(
-        dataset$corte_observado,
-        dbinterface::getfromdb(conn, "corte_observado", id_usina = "BAUFI3")
+        dt,
+        dbinterface::getfromdb(conn_s3, "usinas", id_usina = "BAUFI3")
+    )
+})
+
+test_that("get_corte_observado", {
+    conn_local <- conectamock_pfv(system.file("extdata/", package = "pfvIO"))
+    dt <- get_corte_observado(conn_local, id_usina = "BAUFI3")
+    expect_identical(
+        dt,
+        dbinterface::getfromdb(conn_local, "corte_observado", id_usina = "BAUFI3")
     )
 
+    conn_s3 <- conectamock_pfv("s3://ons-pem-historico/solar/pfvIO-teste")
+    dt <- get_corte_observado(conn_s3, id_usina = "BAUFI3")
     expect_identical(
-        dataset$geracao_observada,
-        dbinterface::getfromdb(conn, "geracao_observada", id_usina = "BAUFI3")
+        dt,
+        dbinterface::getfromdb(conn_s3, "corte_observado", id_usina = "BAUFI3")
+    )
+})
+
+test_that("get_geracao_observada", {
+    conn_local <- conectamock_pfv(system.file("extdata/", package = "pfvIO"))
+    dt <- get_geracao_observada(conn_local, id_usina = "BAUFI3")
+    expect_identical(
+        dt,
+        dbinterface::getfromdb(conn_local, "geracao_observada", id_usina = "BAUFI3")
     )
 
+    conn_s3 <- conectamock_pfv("s3://ons-pem-historico/solar/pfvIO-teste")
+    dt <- get_geracao_observada(conn_s3, id_usina = "BAUFI3")
     expect_identical(
-        dataset$irradiancia_prevista,
-        dbinterface::getfromdb(conn, "irradiancia_prevista", id_usina = "BAUFI3")
+        dt,
+        dbinterface::getfromdb(conn_s3, "geracao_observada", id_usina = "BAUFI3")
+    )
+})
+
+test_that("get_irradiancia_prevista", {
+    conn_local <- conectamock_pfv(system.file("extdata/", package = "pfvIO"))
+    dt <- get_irradiancia_prevista(conn_local, id_usina = "BAUFI3")
+    expect_identical(
+        dt,
+        dbinterface::getfromdb(conn_local, "irradiancia_prevista", id_usina = "BAUFI3")
     )
 
+    conn_s3 <- conectamock_pfv("s3://ons-pem-historico/solar/pfvIO-teste")
+    dt <- get_irradiancia_prevista(conn_s3, id_usina = "BAUFI3")
     expect_identical(
-        dataset$melhor_historico_geracao_sem_cortes,
-        dbinterface::getfromdb(conn, "melhor_historico_geracao_sem_cortes", id_usina = "BAUFI3")
+        dt,
+        dbinterface::getfromdb(conn_s3, "irradiancia_prevista", id_usina = "BAUFI3")
+    )
+})
+
+test_that("get_melhor_historico_geracao_sem_cortes", {
+    conn_local <- conectamock_pfv(system.file("extdata/", package = "pfvIO"))
+    dt <- get_melhor_historico_geracao_sem_cortes(conn_local, id_usina = "BAUFI3")
+    expect_identical(
+        dt,
+        dbinterface::getfromdb(conn_local, "melhor_historico_geracao_sem_cortes", id_usina = "BAUFI3")
     )
 
+    conn_s3 <- conectamock_pfv("s3://ons-pem-historico/solar/pfvIO-teste")
+    dt <- get_melhor_historico_geracao_sem_cortes(conn_s3, id_usina = "BAUFI3")
     expect_identical(
-        dataset$melhor_historico_geracao,
-        dbinterface::getfromdb(conn, "melhor_historico_geracao", id_usina = "BAUFI3")
+        dt,
+        dbinterface::getfromdb(conn_s3, "melhor_historico_geracao_sem_cortes", id_usina = "BAUFI3")
+    )
+})
+
+test_that("get_melhor_historico_geracao", {
+    conn_local <- conectamock_pfv(system.file("extdata/", package = "pfvIO"))
+    dt <- get_melhor_historico_geracao(conn_local, id_usina = "BAUFI3")
+    expect_identical(
+        dt,
+        dbinterface::getfromdb(conn_local, "melhor_historico_geracao", id_usina = "BAUFI3")
     )
 
+    conn_s3 <- conectamock_pfv("s3://ons-pem-historico/solar/pfvIO-teste")
+    dt <- get_melhor_historico_geracao(conn_s3, id_usina = "BAUFI3")
     expect_identical(
-        dataset$potencia_disponivel_observada,
-        dbinterface::getfromdb(conn, "potencia_disponivel_observada", id_usina = "BAUFI3")
+        dt,
+        dbinterface::getfromdb(conn_s3, "melhor_historico_geracao", id_usina = "BAUFI3")
+    )
+})
+
+test_that("get_potencia_disponivel_observada", {
+    conn_local <- conectamock_pfv(system.file("extdata/", package = "pfvIO"))
+    dt <- get_potencia_disponivel_observada(conn_local, id_usina = "BAUFI3")
+    expect_identical(
+        dt,
+        dbinterface::getfromdb(conn_local, "potencia_disponivel_observada", id_usina = "BAUFI3")
     )
 
-    # s3
-
-    conn <- conectamock_pfv("s3://ons-pem-historico/solar/pfvIO-teste")
-    dataset <- get_dataset(conn, id_usina = "BAUFI3")
-
+    conn_s3 <- conectamock_pfv("s3://ons-pem-historico/solar/pfvIO-teste")
+    dt <- get_potencia_disponivel_observada(conn_s3, id_usina = "BAUFI3")
     expect_identical(
-        dataset$usinas,
-        dbinterface::getfromdb(conn, "usinas", id_usina = "BAUFI3")
-    )
-
-    expect_identical(
-        dataset$corte_observado,
-        dbinterface::getfromdb(conn, "corte_observado", id_usina = "BAUFI3")
-    )
-
-    expect_identical(
-        dataset$geracao_observada,
-        dbinterface::getfromdb(conn, "geracao_observada", id_usina = "BAUFI3")
-    )
-
-    expect_identical(
-        dataset$irradiancia_prevista,
-        dbinterface::getfromdb(conn, "irradiancia_prevista", id_usina = "BAUFI3")
-    )
-
-    expect_identical(
-        dataset$melhor_historico_geracao_sem_cortes,
-        dbinterface::getfromdb(conn, "melhor_historico_geracao_sem_cortes", id_usina = "BAUFI3")
-    )
-
-    expect_identical(
-        dataset$melhor_historico_geracao,
-        dbinterface::getfromdb(conn, "melhor_historico_geracao", id_usina = "BAUFI3")
-    )
-
-    expect_identical(
-        dataset$potencia_disponivel_observada,
-        dbinterface::getfromdb(conn, "potencia_disponivel_observada", id_usina = "BAUFI3")
+        dt,
+        dbinterface::getfromdb(conn_s3, "potencia_disponivel_observada", id_usina = "BAUFI3")
     )
 })
 
